b159cc189c282213ca25688b80b469e444f6bcb8
Engels Antero <engelsantero@gmail.com>
2023-02-14 18:24:33 -0300

    Update valida_login.php

--- valida_login.php
+++ valida_login.php
@@ -3,16 +3,24 @@
 	session_start();
 
 	$usuario_autenticado = false;
+	$usuario_id = null;
+	$usuario_perfil_id = null;
+
+	$perfis = ['administrativo'=>1,'usuario'=>2];
 
 	$lista_usuarios = array(
-		array('email'=>'adm@teste.com', 'senha'=>'123456'),
-		array('email'=>'user@teste.com', 'senha'=>'abcdef')
+		array('id'=>1, 'email'=>'adm@teste.com', 'senha'=>'1234', 'perfil_id'=>1),
+		array('id'=>2, 'email'=>'user@teste.com', 'senha'=>'1234', 'perfil_id'=>1),
+		array('id'=>3, 'email'=>'jose@teste.com', 'senha'=>'1234', 'perfil_id'=>2),
+		array('id'=>4, 'email'=>'maria@teste.com', 'senha'=>'1234', 'perfil_id'=>2)
 	);
 
 	foreach($lista_usuarios as $usuario){
 
 		if($usuario['email'] == $_POST['email'] && $usuario['senha'] == $_POST['senha']){
 			$usuario_autenticado = true;
+			$usuario_id = $usuario['id'];
+			$usuario_perfil_id = $usuario['perfil_id'];
 		}
 
 	}
@@ -20,6 +28,8 @@
 	if($usuario_autenticado){
 		echo 'Usuario autenticado com sucesso';
 		$_SESSION['autenticado'] = 'SIM';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_id'] = $usuario_perfil_id;
 		header('Location: home.php');
 	}else {
 		$_SESSION['autenticado'] = 'NAO';
